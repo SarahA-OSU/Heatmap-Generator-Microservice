5d73cf45bfd2c6ca0d997c2f53fec2ab93e50070
sarahangle <SarahAngleEngineer@gmail.com>
2025-05-19 21:29:17 -0700

    Update some comments and remove excess import

--- Client/Heatmap_Client.py
+++ Client/Heatmap_Client.py
@@ -1,9 +1,8 @@
 import socket
 import os
-import time
 
-HOST = '3.131.47.90'
 # HOST = 'localhost'
+HOST = '3.131.47.90'
 UPLOAD_PORT = 5555
 REQUEST_PORT = 5556
 SEPERATOR = "<SEPARATOR>"
@@ -15,64 +14,63 @@ def pad_string(text, length=MESSAGE_SIZE, char=' '):
 
 def initUploadSocket():
     s = socket.socket()
-    print(f"Connecting to {HOST}:{UPLOAD_PORT}")
     s.connect((HOST, UPLOAD_PORT))
-    print(f"Connected")
+    print(f"Connected to file upload socket at {HOST}:{UPLOAD_PORT}")
     return s
 
 def uploadFile(socket, requestFileName):
     fileSize = os.path.getsize(requestFileName)
     message = requestFileName + SEPERATOR + str(fileSize)
+    print('I sent a message to the file upload server: ' + message)
     message = pad_string(message)
     socket.send(message.encode())
 
+    print('I sent a file to the file upload server: ' + requestFileName)
     with open(requestFileName, "rb") as f:
         while True:
-            # read the bytes from the file
+            # read 4kB bytes from the file
             bytes_read = f.read(BUFFER_SIZE)
             if not bytes_read:
                 # file transmitting is done
                 break
-            # we use sendall to assure transimission in busy networks
             socket.sendall(bytes_read)
 
 def initRequestSocket():
     s = socket.socket()
-    print(f"Connecting to {HOST}:{REQUEST_PORT}")
     s.connect((HOST, REQUEST_PORT))
-    print(f"Connected")
+    print(f"Connected to request socket at {HOST}:{REQUEST_PORT}")
     return s
 
 def requestHeatmap(socket, requestFileName, colorString):
-    # Request that the server make a heatmap
     message = requestFileName + SEPERATOR + colorString
+    print('I sent a message to the request server: ' + message)
     message = pad_string(message)
     socket.send(message.encode())
 
 def isReplySuccessful(socket):
-    # Receive some confirmation back from service that it will generate heat map with these inputs
     reply = socket.recv(MESSAGE_SIZE).decode().strip()
+    print('I received a message from the request server: ' + reply)
     if reply[:2] != '00':
-        print('Error: ' +  reply[3:])
         return False
     else:
-        print('Request successful')
         return True
 
 def receiveFile(socket, newFilename):
-    received = socket.recv(MESSAGE_SIZE).decode()
+    received = socket.recv(MESSAGE_SIZE).decode().strip()
+    print('I received a message from the request server: ' + received)
     filename, filesize = received.split(SEPERATOR)
 
     with open(newFilename, "wb") as f:
         while True:
-            # read 1024 bytes from the socket (receive)
+            # read 4kB from the socket (receive)
             bytes_read = socket.recv(BUFFER_SIZE)
             if not bytes_read:
-                # nothing is received
                 # file transmitting is done
                 break
             # write to the file the bytes we just received
             f.write(bytes_read)
+
+    print('I received a file from the request server and wrote it to: ' + newFilename)
     return
 
 def getHeatmap(dataFile, imageFileName, colorString):
@@ -87,5 +85,5 @@ def getHeatmap(dataFile, imageFileName, colorString):
     requestSocket.close()
 
 if __name__ == "__main__":
-    # getHeatmap("ExampleData03.csv", "ReturnedImage03.png", "#00FF00 #FFFF00 #0000FF")
-    getHeatmap("ExampleData01.csv", "ReturnedImage01.png", '')
+    getHeatmap("ExampleData01.csv", "ReturnedImage01.png", "#DD00DD #EE88EE #FFFFFF")
+    # getHeatmap("ExampleData03.csv", "ReturnedImage03.png", '')
